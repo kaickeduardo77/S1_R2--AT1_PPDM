fd94969c4e493bdc51375622dd2323e98414022f
KAICK EDUARDO ARAUJO DA SILVA <kaickeduardo415@gmail.com>
2026-03-09 08:44:24 -0300

    correçaoo

--- App.js
+++ App.js
@@ -5,11 +5,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       
-      <View style={[styles.box, { backgroundColor: "red" }]} />
+      <View style={styles.box1}></View>
 
-      <View style={[styles.box, { backgroundColor: "blue" }]} />
-      
-      <View style={[styles.box, { backgroundColor: "green" }]} />
+      <View style={styles.box2} ></View>
+
+      <View style={styles.box3} ></View>
 
     </View>
   );
@@ -24,8 +24,22 @@ const styles = StyleSheet.create({
     padding: 20
   },
 
-  box: {
+  box1: {
     width: 80,
-    height: 80
+    height: 80,
+    backgroundColor: 'red'
+  },
+
+  
+  box2 : {
+    width: 80,
+    height: 80,
+    backgroundColor: 'blue'
+  },
+  
+  box3: {
+    width: 80,
+    height: 80,
+    backgroundColor: 'green'
   }
 });
